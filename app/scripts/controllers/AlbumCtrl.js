fa645b8d17030f2eb1496d494285abd9bbcfa0ed
Deb Strickland <debfrombc@gmail.com>
2017-07-01 21:12:28 -0700

    album data tied to controller

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,14 @@
 (function() {
-  function AlbumCtrl($scope) {
-    $scope.albumData = album70Themes;
-    // this.albumData = album70Themes;
+  // function AlbumCtrl($scope) {
+  //   $scope.albumData = album70Themes;
+  //   // this.albumData = album70Themes;
+  // }
+  function AlbumCtrl(Fixtures) {
+    this.albumData = Fixtures.getAlbum();
   }
 
   angular
     .module('blocJams')
-    .controller('AlbumCtrl', AlbumCtrl);
+    // add service recipe Fixtures as a dependency to access its data
+    .controller('AlbumCtrl', ['Fixtures', AlbumCtrl] );
 })();
--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -11,11 +11,11 @@
       year: '1881',
       albumArtUrl: '/assets/images/album_covers/01.png',
       songs: [
-        { title: 'Blue', duration: '4:26', audioUrl: 'assets/music/blue' },
-        { title: 'Green', duration: '3:14', audioUrl: 'assets/music/green' },
-        { title: 'Red', duration: '5:01', audioUrl: 'assets/music/red' },
-        { title: 'Pink', duration: '3:21', audioUrl: 'assets/music/pink' },
-        { title: 'Magenta', duration: '2:15', audioUrl: 'assets/music/magenta' }
+        { title: 'Blue', duration: '4:26', audioUrl: '/assets/music/blue' },
+        { title: 'Green', duration: '3:14', audioUrl: '/assets/music/green' },
+        { title: 'Red', duration: '5:01', audioUrl: '/assets/music/red' },
+        { title: 'Pink', duration: '3:21', audioUrl: '/assets/music/pink' },
+        { title: 'Magenta', duration: '2:15', audioUrl: '/assets/music/magenta' }
       ]
     };
 
@@ -27,30 +27,19 @@
       year: '1973-1978',
       albumArtUrl: '/assets/images/album_covers/70s-cover.jpg',
       songs: [
-        { title: 'Green Acres', duration: 63.56, audioUrl: 'assets/music/grnacres' },
-        { title: 'Andy Griffith Show', duration: 50.22, audioUrl: 'assets/music/andgrif' },
-        { title: 'Cheers', duration: 62.56, audioUrl: 'assets/music/cheers' },
-        { title: "Gilligan's Island", duration: 90.91, audioUrl: 'assets/music/gilligan' },
-        { title: 'Batman', duration: 43.27, audioUrl: 'assets/music/batman' },
-        { title: 'Flintstones', duration: 43.27, audioUrl: 'assets/music/fstones' },
-        { title: 'I Dream of Jeannie', duration: 43.27, audioUrl: 'assets/music/jeannie' }
+        { title: 'Green Acres', duration: 63.56, audioUrl: '/assets/music/grnacres' },
+        { title: 'Andy Griffith Show', duration: 50.22, audioUrl: '/assets/music/andgrif' },
+        { title: 'Cheers', duration: 62.56, audioUrl: '/assets/music/cheers' },
+        { title: "Gilligan's Island", duration: 90.91, audioUrl: '/assets/music/gilligan' },
+        { title: 'Batman', duration: 43.27, audioUrl: '/assets/music/batman' },
+        { title: 'Flintstones', duration: 43.27, audioUrl: '/assets/music/fstones' },
+        { title: 'I Dream of Jeannie', duration: 43.27, audioUrl: '/assets/music/jeannie' }
       ]
     };
 
-    // another example album
-    var albumMarconi = {
-      title: 'The Telephone',
-      artist: 'Guglielmo Marconi',
-      label: 'EM',
-      year: '1909',
-      albumArtUrl: '/assets/images/album_covers/20.png',
-      songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21' },
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15' }
-      ]
+    // expose album data to controllers
+    Fixtures.getAlbum = function() {
+      return album70Themes;
     };
 
     return Fixtures;
