bc1cc1fa773f846e59d7fc34551c923af0890f70
Deb Strickland <debfrombc@gmail.com>
2017-07-09 17:33:36 -0700

    implement song play

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,33 @@
 (function() {
   function SongPlayer() {
+    var currentSong = null;
+    var currentBuzzObject = null;
+
     var play = function(song) {
-      var currentBuzzObject = new buzz.sound(song.audioUrl, {
+      // test if a selected song is the one now playing
+      if ( currentSong !== song ) {
+        // 1. Stop the currently playing song, if there is one
+        // 2. Set a new Buzz sound object
+        // 3. Set the newly chosen song object as the currentSong
+        // 4. Play the new Buzz sound object
+
+        // user chose diff song, stop this one from playing
+        if ( currentBuzzObject ) {
+          currentBuzzObject.stop();
+        }
+      } else if ( currentSong === song ) {
+        // yes, selected song is chosen, it must be paused, so play it
+        if ( currentBuzzObject.isPaused() ) {
+          currentBuzzObject.play();
+        }
+      }
+
+      currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
       });
 console.log('in SongPlayer constructor, before play()');
+      currentSong = song;   // switch songs
       currentBuzzObject.play();
     };
 
