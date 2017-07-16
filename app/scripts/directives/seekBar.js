c8c1641e9ed7bc16c739cd70112bd0c9c6e2de24
Deb Strickland <debfrombc@gmail.com>
2017-07-16 01:05:13 -0700

    thumb moves with click seekbar

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -30,18 +30,19 @@
       offsetXPercent = Math.min(1, offsetXPercent);
 
       return offsetXPercent;
-   };
+    };
 
     return {
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
-      restrict: 'E',
-      scope: { }, // 'isolated scope' new scope for this directive
+      restrict: 'EA',   // added 'A' attribute for thumb
+      scope: { }, // 'isolated scope' new scope for directive
       link: function(scope, element, attributes) {
         // need posn of thumb and width of seek bar playback to determine
         //    how much of song is remaining to be played.
         scope.value = 0;
         scope.max = 100;
+        scope.vol_start = 38;   // initial postion for vol control thumb
 
         var seekBar = $(element);
 
@@ -50,11 +51,8 @@
           var value = scope.value;
           var max = scope.max;
           var percent = value / max * 100;
+
           return percent + "%";
-        };
-
-        var updatePosition = function() {
-
         };
 
 
@@ -66,26 +64,28 @@
 
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
+
           scope.value = percent * scope.max;
         };
 
+        // assignment
         scope.thumbStyle = function() {
-          // formatted thumb bar width to show adjusted volume
-          //return { width: percentString() };
+          // reformat thumb portion of fill bar to show adjusted position
+          return { left: percentString() };
         };
 
 
         /**
-         * @function trackThumb - Public
+         * @function trackThumb() - Public
          * @desc Similar to scope.onClickSeekBar, but uses $apply to constantly apply the change in value of  scope.value as the user drags the seek bar thumb.
          */
         scope.trackThumb = function() {
           $document.bind('mousemove.thumb', function(event) {
             var percent = calculatePercent(seekBar, event);
               scope.$apply(function() {
-              scope.value = percent * scope.max;
+                scope.value = percent * scope.max;
+              });
             });
-          });
 
           $document.bind('mouseup.thumb', function() {
             $document.unbind('mousemove.thumb');
